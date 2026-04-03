34067081aba583b41bb1a5843824a14167e54158
kalidashodekare14 <kalidashodekare0@gmail.com>
2026-04-04 01:24:41 +0600

    Home page GetStarted section implemented

---
+++ components/sections/home/GetStarted.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { motion } from "motion/react";
+import Link from "next/link";
+
+const GetStarted = () => {
+  return (
+    <div className="2xl:w-350 xl:w-310 lg:w-260 w-full m-auto px-5 lg:px-0 bg-[#ffdcec] rounded-2xl py-20 flex justify-around items-center my-10">
+      <div className="space-y-3">
+        <h1 className="text-3xl font-bold">Ready to get started?</h1>
+        <p className="text-charcoal">
+          Join 50,000+ users who already simplified how they hire home services.
+        </p>
+      </div>
+      <div className="space-x-3">
+        <Link href={"/signin"}>
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            className="btn w-40 h-10 border border-[#F72585] text-black rounded-xl cursor-pointer"
+          >
+            Login
+          </motion.button>
+        </Link>
+        <Link href={"/signin"}>
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            className="btn w-40 h-10 bg-[#F72585] border-0 text-white rounded-xl cursor-pointer"
+          >
+            Get Started
+          </motion.button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default GetStarted;
--- components/templates/HomeTemplate/HomeTemplate.tsx
+++ components/templates/HomeTemplate/HomeTemplate.tsx
@@ -3,6 +3,7 @@
 import BannerSection from "@/components/sections/home/BannerSection";
 import CategorySection from "@/components/sections/home/CategorySection";
 import GetServices from "@/components/sections/home/GetServices";
+import GetStarted from "@/components/sections/home/GetStarted";
 import OurAchievements from "@/components/sections/home/OurAchievements";
 import SmartGuard from "@/components/sections/home/SmartGuard";
 import Testimonial from "@/components/sections/home/Testimonial";
@@ -16,6 +17,7 @@ const HomeTemplate = () => {
       <SmartGuard />
       <OurAchievements />
       <Testimonial />
+      <GetStarted />
     </div>
   );
 };
